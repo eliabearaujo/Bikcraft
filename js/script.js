962b097b9591f6760f1d6c4e785b0871d1631c96
eliabearaujo <eliabe_trajano@hotmail.com>
2022-06-17 02:44:14 -0300

    Add plugin animação

--- js/script.js
+++ js/script.js
@@ -24,3 +24,48 @@ function ativarProduto(parametro) {
   console.log(elemento);
 }
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controle = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controle);
+
+  resposta.classList.toggle("ativo");
+  const ativo = resposta.classList.contains("ativo");
+  pergunta.setAttribute("aria-expanded", ativo);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+  0;
+}
